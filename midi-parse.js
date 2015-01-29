21e254491286e838c0b386b874adcdbf5fa5d888
Weston Mizumoto <westonmizumoto@DN0a22a3bf.SUNet>
2015-01-28 16:19:04 -0800

    actually uploads files now

--- midi-parse.js
+++ midi-parse.js
@@ -1,30 +1,42 @@
-var fileInput = $('#files');
-var uploadButton = $('#upload');
-
-uploadButton.on('click', function() {
-    if (!window.FileReader) {
-        alert('Your browser is not supported')
-    }
-    var input = fileInput.get(0);
-    
-    // Create a reader object
-    var reader = new FileReader();
-    if (input.files.length) {
-        var textFile = input.files[0];
-        reader.readAsText(textFile);
-        $(reader).on('load', processFile);
+var reader; //GLOBAL File Reader object
+function checkFileAPI() {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        reader = new FileReader();
+        return true; 
     } else {
-        alert('Please upload a file before continuing')
-    } 
-});
-
-function processFile(e) {
-    var file = e.target.result,
-        results;
-    if (file && file.length) {
-        results = file.split("\n");
-        console.log(results)
-        $('#name').val(results[0]);
-        $('#age').val(results[1]);
+        alert('The File APIs are not fully supported by your browser. Fallback required.');
+        return false;
     }
 }
+
+/**
+ * read text input
+ */
+function readText(filePath) {
+    var output = "";
+    if(filePath.files && filePath.files[0]) {           
+        reader.onload = function (e) {
+            output = e.target.result;
+            displayContents(output);
+        };
+        reader.readAsBinaryString(filePath.files[0]);
+    }
+    return true;
+}   
+
+/**
+ * display content using a basic HTML replacement
+ */
+function displayContents(txt) {
+	var bytes = [];
+
+	for (var i = 0; i < txt.length; ++i)
+	{
+	    bytes.push(txt.charCodeAt(i));
+	}
+
+	alert(bytes); // 72,0,101,0,108,0,108,0,111,0
+
+    // var el = document.getElementById('main'); 
+    // el.innerHTML = txt; //display output in DOM
+} 
